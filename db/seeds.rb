8f13e0c61c15764f7d96d31f331979f796b2ba12
Andrew Tinekov <allexus39@gmail.com>
2019-11-23 00:28:38 +0300

    Add language_code to seeds and fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 # Create countries
-Country.create!(name: 'Россия', iso_code: 'RU')
-Country.create!(name: 'Slovenia', iso_code: 'SI')
-Country.create!(name: 'Turkey', iso_code: 'TR')
+Country.create!(iso_code: 'RU', language_code: 'ru', name: 'Россия')
+Country.create!(iso_code: 'SI', language_code: 'sl', name: 'Slovenia')
+Country.create!(iso_code: 'TR', language_code: 'tr', name: 'Turkey')
