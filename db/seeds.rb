f90df981089d006245170be9567961b9774cb1d3
Andrew Tinekov <allexus39@gmail.com>
2019-11-28 17:29:06 +0300

    Refactor seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 # Create countries
-Country.create!(iso_code: 'RU', language_code: 'ru', name: 'Россия')
-Country.create!(iso_code: 'SI', language_code: 'sl', name: 'Slovenia')
-Country.create!(iso_code: 'TR', language_code: 'tr', name: 'Turkey')
+russia = Country.create!(iso_code: 'RU', language_code: 'ru', name: 'Россия')
+slovenia = Country.create!(iso_code: 'SI', language_code: 'sl', name: 'Slovenia')
+turkey = Country.create!(iso_code: 'TR', language_code: 'tr', name: 'Turkey')
 
 # Create subdivisions
-Subdivision.create!(iso_code: 'RU-MOW', local_code: '45', name: 'Москва', country: Country.find_by!(language_code: 'ru'))
-Subdivision.create!(iso_code: 'SI-061', local_code: '61', name: 'Ljubljana', country: Country.find_by!(language_code: 'sl'))
-Subdivision.create!(iso_code: 'TR-34', local_code: '34', name: 'İstanbul', country: Country.find_by!(language_code: 'tr'))
+istanbul = Subdivision.create!(iso_code: 'TR-34', local_code: '34', name: 'İstanbul', country: turkey)
+ljubljana = Subdivision.create!(iso_code: 'SI-061', local_code: '61', name: 'Ljubljana', country: slovenia)
+moscow = Subdivision.create!(iso_code: 'RU-MOW', local_code: '45', name: 'Москва', country: russia)
