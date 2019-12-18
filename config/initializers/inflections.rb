a87ca96a7d4ebac74b6d0269b27a940b7cb57ee1
Andrew Tinekov <allexus39@gmail.com>
2019-12-18 11:24:06 +0300

    ADD acronyms for countries

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -13,4 +13,7 @@
 # These inflection rules are supported but not enabled by default:
 ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.acronym 'API'
+  inflect.acronym 'RU'
+  inflect.acronym 'SI'
+  inflect.acronym 'TR'
 end
