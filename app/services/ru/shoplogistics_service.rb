c0e68932e021238ad494fa08764a00f8ce5bd311
Andrew Tinekov <allexus39@gmail.com>
2019-12-20 14:17:35 +0300

    Update request body in shoplogistics service

--- app/services/ru/shoplogistics_service.rb
+++ app/services/ru/shoplogistics_service.rb
@@ -2,16 +2,17 @@ class RU::ShoplogisticsService
   BASE_URI = 'https://client-shop-logistics.ru/index.php?route=deliveries/api'
   HEADERS_PARAMS = { 'Content-Type' => 'application/x-www-form-urlencoded; charset=utf-8' }
   FETCH_DELIVERY_INFO_REQUEST_BODY = {
-    api_id: Rails.application.credentials.dig(:ru, :shoplogistics, :api_token),
     function: 'get_deliveries_tarifs',
+    api_id: Rails.application.credentials.dig(:ru, :shoplogistics, :api_token),
     from_city: 'Москва',
-    num: '1',
+    to_city: '',
     weight: '1',
-    order_height: '',
     order_length: '',
     order_width: '',
+    order_height: '',
     order_price: '1000.00',
-    ocen_price: '1000.00'
+    ocen_price: '1000.00',
+    num: '1'
   }
 
   attr_reader :locality, :request_body
@@ -40,6 +41,6 @@ class RU::ShoplogisticsService
   end
 
   def encoded_request_body
-    CGI.escape(Base64.strict_encode64(request_body.to_xml(root: :request, skip_instruct: true)))
+    URI.escape(Base64.encode64(request_body.to_xml(root: :request, skip_instruct: true)))
   end
 end
