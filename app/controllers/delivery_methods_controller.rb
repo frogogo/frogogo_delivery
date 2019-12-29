48d799685dcbb68d0da3bba39e30a4987a463c92
Andrew Tinekov <allexus39@gmail.com>
2019-12-29 13:13:16 +0300

    Return not_found instead no_content if delivery_methods blank

--- app/controllers/delivery_methods_controller.rb
+++ app/controllers/delivery_methods_controller.rb
@@ -2,7 +2,7 @@ class DeliveryMethodsController < ApplicationController
   def show
     @delivery_methods = DeliveryMethod.search(search_params)
 
-    return head :no_content if @delivery_methods.blank?
+    return head :not_found if @delivery_methods.blank?
   end
 
   private
