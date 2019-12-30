ac18bf222b212c7ce73d73e23d6acb277c139ae0
Andrew Tinekov <allexus39@gmail.com>
2019-12-30 18:05:12 +0300

    Fix code style

--- app/models/concerns/dateable.rb
+++ app/models/concerns/dateable.rb
@@ -9,7 +9,9 @@ module Dateable
       @estimate_delivery_date = Date.current + date_interval.last.to_i.days
       # +1 day if Time.current > 4pm
       @estimate_delivery_date += 1.day if Time.current > Time.current.middle_of_day + 4.hours
-      @estimate_delivery_date = @estimate_delivery_date.next_weekday if @estimate_delivery_date.on_weekday?
+      if @estimate_delivery_date.on_weekday?
+        @estimate_delivery_date = @estimate_delivery_date.next_weekday
+      end
     end
 
     @estimate_delivery_date
