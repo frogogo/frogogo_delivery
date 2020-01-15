e5c84400fdb9b4bc1fb95cbebc0733c1734f5d4c
Andrew Tinekov <allexus39@gmail.com>
2020-01-15 17:58:42 +0300

    Show only uniq delivery points
    - uniq by latitude and longitude

--- app/controllers/delivery_methods_controller.rb
+++ app/controllers/delivery_methods_controller.rb
@@ -3,7 +3,11 @@ class DeliveryMethodsController < ApplicationController
     @delivery_methods = DeliveryMethodsResolver.new(search_params).resolve
     return head :not_found if @delivery_methods.blank?
 
-    @delivery_points = DeliveryPoint.active.where(delivery_method: @delivery_methods)
+    @delivery_points = DeliveryPoint
+      .active
+      .where(delivery_method: @delivery_methods)
+      .uniq { |delivery_point| [delivery_point.latitude, delivery_point.longitude] }
+
     @delivery_zone = @delivery_methods.first.deliverable.delivery_zone
     return head :not_found if @delivery_zone.blank?
   end
