38b0d3e5039543f2df0a735b90f14f638f8063a6
Andrew Tinekov <allexus39@gmail.com>
2020-01-15 11:00:27 +0300

    Revert "Filter delivery methods regarding plan(courier method only)"
    This reverts commit 8656d29f49978678ce94e4514811adbd640fed84.

--- app/controllers/delivery_methods_controller.rb
+++ app/controllers/delivery_methods_controller.rb
@@ -3,7 +3,6 @@ class DeliveryMethodsController < ApplicationController
     @delivery_methods = DeliveryMethodsResolver.new(search_params).resolve
     return head :not_found if @delivery_methods.blank?
 
-    @delivery_methods = DeliveryMethodsFilter.new(@delivery_methods).filter
     @delivery_points = DeliveryPoint.active.where(delivery_method: @delivery_methods)
     @delivery_zone = @delivery_methods.first.deliverable.delivery_zone
     return head :not_found if @delivery_zone.blank?
