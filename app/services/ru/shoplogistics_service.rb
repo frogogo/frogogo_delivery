d5c96d35c62e07c37cd6775270b727dacaf4823a
Andrew Tinekov <allexus39@gmail.com>
2020-03-04 16:07:06 +0300

    Revert "SL: Skip termal pickup points"
    This reverts commit bb8c360ff80feef4eb88261308237aaf93cafe14.

--- app/services/ru/shoplogistics_service.rb
+++ app/services/ru/shoplogistics_service.rb
@@ -40,7 +40,6 @@ class RU::ShopLogisticsService < DeliveryService
 
         begin
           pickup_delivery_method(tarif['srok_dostavki'])
-          next if tarif['is_terminal'] == '1'
 
           @pickup_delivery_method.delivery_points.create!(
             address: format_string(tarif['address']),
