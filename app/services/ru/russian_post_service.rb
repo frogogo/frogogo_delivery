01d397c5fa13b9f8f5637efb6dc75a99d913f76f
Polina Janochkina <apollinariya2209@yandex.ru>
2020-07-29 14:35:17 +0300

    Create RussianPostService

---
+++ app/services/ru/russian_post_service.rb
@@ -0,0 +1,46 @@
+class RU::RussianPostService < DeliveryService
+  RUSSIAN_POST_NAME = 'RussianPost'
+
+  def initialize(locality)
+    super
+
+    @delivery_service = RU::RussianPostAdapter.new(locality)
+    @provider = Provider.find_by(name: RUSSIAN_POST_NAME)
+  end
+
+  def fetch_delivery_info
+    return unless super
+
+    # Get unique points by address to avoid 'PG::UniqueViolation'
+    @response = delivery_service.post_offices_list.uniq do |post_office|
+      [post_office['address-source']]
+    end
+
+    save_data
+  end
+
+  private
+
+  def save_data
+    response.each do |post_office|
+      next if post_office['is-temporary-closed'] == true
+
+      pickup_delivery_method.delivery_points.create!(
+        address: "#{post_office['address-source']}, #{post_office['settlement']}",
+        latitude: post_office['latitude'],
+        longitude: post_office['longitude'],
+        name: post_office['address-source'],
+        working_hours: post_office['working-hours']
+      )
+    end
+  end
+
+  def pickup_delivery_method
+    @pickup_delivery_method ||=
+      DeliveryMethod.create_or_find_by!(
+        method: :pickup,
+        deliverable: locality,
+        provider: provider
+      )
+  end
+end
