5de27e06d60120ff7316f5e2db0b3ac9fb7cd634
Polina Janochkina <apollinariya2209@yandex.ru>
2020-07-29 12:38:20 +0300

    RU::RussianPostAdapter: use Dadata and get points by coordinates

--- app/lib/ru/russian_post_adapter.rb
+++ app/lib/ru/russian_post_adapter.rb
@@ -1,54 +1,71 @@
 class RU::RussianPostAdapter < DeliveryAdapter
-  POSTAL_CODES_URI = 'https://otpravka-api.pochta.ru/postoffice/1.0/settlement.offices.codes'
-  POST_OFFICE_URI = 'https://otpravka-api.pochta.ru/postoffice/1.0/'
+  POST_OFFICE_URI = 'https://otpravka-api.pochta.ru/postoffice/1.0/nearby'
+  DADATA_URI = 'https://cleaner.dadata.ru/api/v1/clean/address'
   HEADERS = {
     'Content-Type' => 'application/json; charset=UTF-8',
     'Authorization' => '',
-    'X-User-Authorization' => ''
+    'X-User-Authorization' => '',
+    'X-Secret' => ''
   }
   QUERY = {
-    'settlement' => '',
-    'region' => ''
+    'latitude' => '',
+    'longitude' => '',
+    'top' => '4000',
+    'filter' => 'ALL',
+    'filter-by-office-type' => 'true'
   }
 
-  def postal_codes
-    request_postal_codes ||= HTTParty.get(
-      POSTAL_CODES_URI,
-      headers: HEADERS.merge(
-        'Authorization': "AccessToken #{api_token}",
-        'X-User-Authorization': "Basic #{api_key}"
-      ),
-      query: QUERY.merge(settlement: locality.name, region: locality.subdivision.name)
-    )
-
-    request_postal_codes.parsed_response
-  end
-
   def post_offices_list
-    post_offices_list = {}
-
-    postal_codes.each do |postal_code|
-      request_post_offices = HTTParty.get(
-        POST_OFFICE_URI + postal_code.to_s,
-        headers: HEADERS.merge(
-          'Authorization': "AccessToken #{api_token}",
-          'X-User-Authorization': "Basic #{api_key}"
-        )
-      )
-
-      post_offices_list = post_offices_list.merge(request_post_offices.parsed_response)
-    end
-
-    post_offices_list
+    request_post_offices.parsed_response
   end
 
   private
 
-  def api_token
+  def post_api_token
     Rails.application.credentials.dig(:ru, :russian_post, :api_token)
   end
 
-  def api_key
+  def post_api_key
     Rails.application.credentials.dig(:ru, :russian_post, :api_key)
+  end
+
+  def dadata_api_token
+    Rails.application.credentials.dig(:ru, :dadata, :api_token)
+  end
+
+  def dadata_secret_key
+    Rails.application.credentials.dig(:ru, :dadata, :secret_key)
+  end
+
+  def request_post_offices
+    HTTParty.get(
+      POST_OFFICE_URI,
+      headers: HEADERS.merge(
+        'Authorization': "AccessToken #{post_api_token}",
+        'X-User-Authorization': "Basic #{post_api_key}"
+      ),
+      query: QUERY.merge('latitude' => locality_latitude, 'longitude' => locality_longitude)
+    )
+  end
+
+  def request_locality_coordinates
+    coordinates = HTTParty.post(
+      DADATA_URI,
+      headers: HEADERS.merge(
+        'Authorization': "Token #{dadata_api_token}",
+        'X-Secret': "#{dadata_secret_key}"
+      ),
+      body: [locality.subdivision.name, locality.name].to_json
+    )
+
+    coordinates.parsed_response
+  end
+
+  def locality_longitude
+    request_locality_coordinates[1]['geo_lon']
+  end
+
+  def locality_latitude
+    request_locality_coordinates[1]['geo_lat']
   end
 end
