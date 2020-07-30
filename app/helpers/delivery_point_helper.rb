cae394e54f232886bc5525f7816001a4e8ce19e4
Polina Janochkina <apollinariya2209@yandex.ru>
2020-07-30 17:25:43 +0300

    Update working_hours display

--- app/helpers/delivery_point_helper.rb
+++ app/helpers/delivery_point_helper.rb
@@ -9,51 +9,51 @@ module DeliveryPointHelper
       when 1
         result += 'пн'
         if hours['begin-worktime'].nil?
-          result += ' — выходной; '
+          result += ' — выходной, '
         else
-          result += ": c #{Time.zone.parse(hours['begin-worktime']).strftime('%H:%M')} до #{Time.zone.parse(hours['end-worktime']).strftime('%H:%M')}; "
+          result += ": #{Time.zone.parse(hours['begin-worktime']).strftime('%H:%M')}-#{Time.zone.parse(hours['end-worktime']).strftime('%H:%M')}, "
         end
       when 2
         result += 'вт'
         if hours['begin-worktime'].nil?
-          result += ' — выходной; '
+          result += ' — выходной, '
         else
-          result += ": c #{Time.zone.parse(hours['begin-worktime']).strftime('%H:%M')} до #{Time.zone.parse(hours['end-worktime']).strftime('%H:%M')}; "
+          result += ": #{Time.zone.parse(hours['begin-worktime']).strftime('%H:%M')}-#{Time.zone.parse(hours['end-worktime']).strftime('%H:%M')}, "
         end
       when 3
         result += 'ср'
         if hours['begin-worktime'].nil?
-          result += ' — выходной; '
+          result += ' — выходной, '
         else
-          result += ": c #{Time.zone.parse(hours['begin-worktime']).strftime('%H:%M')} до #{Time.zone.parse(hours['end-worktime']).strftime('%H:%M')}; "
+          result += ": #{Time.zone.parse(hours['begin-worktime']).strftime('%H:%M')}-#{Time.zone.parse(hours['end-worktime']).strftime('%H:%M')}, "
         end
       when 4
         result += 'чт'
         if hours['begin-worktime'].nil?
-          result += ' — выходной; '
+          result += ' — выходной, '
         else
-          result += ": c #{Time.zone.parse(hours['begin-worktime']).strftime('%H:%M')} до #{Time.zone.parse(hours['end-worktime']).strftime('%H:%M')}; "
+          result += ": #{Time.zone.parse(hours['begin-worktime']).strftime('%H:%M')}-#{Time.zone.parse(hours['end-worktime']).strftime('%H:%M')}, "
         end
       when 5
         result += 'пт'
         if hours['begin-worktime'].nil?
-          result += ' — выходной; '
+          result += ' — выходной, '
         else
-          result += ": c #{Time.zone.parse(hours['begin-worktime']).strftime('%H:%M')} до #{Time.zone.parse(hours['end-worktime']).strftime('%H:%M')}; "
+          result += ": #{Time.zone.parse(hours['begin-worktime']).strftime('%H:%M')}-#{Time.zone.parse(hours['end-worktime']).strftime('%H:%M')}, "
         end
       when 6
         result += 'сб'
         if hours['begin-worktime'].nil?
-          result += ' — выходной; '
+          result += ' — выходной, '
         else
-          result += ": c #{Time.zone.parse(hours['begin-worktime']).strftime('%H:%M')} до #{Time.zone.parse(hours['end-worktime']).strftime('%H:%M')}; "
+          result += ": #{Time.zone.parse(hours['begin-worktime']).strftime('%H:%M')}-#{Time.zone.parse(hours['end-worktime']).strftime('%H:%M')}, "
         end
       when 7
         result += 'вс'
         if hours['begin-worktime'].nil?
-          result += ' — выходной; '
+          result += ' — выходной '
         else
-          result += ": c #{Time.zone.parse(hours['begin-worktime']).strftime('%H:%M')} до #{Time.zone.parse(hours['end-worktime']).strftime('%H:%M')};"
+          result += ": #{Time.zone.parse(hours['begin-worktime']).strftime('%H:%M')}-#{Time.zone.parse(hours['end-worktime']).strftime('%H:%M')}"
         end
       end
     end
