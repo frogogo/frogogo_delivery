96e5bdde4021c8d107299a9872265da6e3cb6a98
Polina Janochkina <apollinariya2209@yandex.ru>
2020-07-30 19:37:49 +0300

    Create points only for ГОПС

--- app/services/ru/russian_post_service.rb
+++ app/services/ru/russian_post_service.rb
@@ -23,8 +23,8 @@ class RU::RussianPostService < DeliveryService
 
   def save_data
     response.each do |post_office|
-      next unless post_office['settlement'] == locality.name && post_office['region'] == locality.subdivision.name
-      next if post_office['type-code'] == 'ПОЧТОМАТ'
+      next unless post_office['settlement'] == locality.name
+      next unless post_office['type-code'] == 'ГОПС'
       next if post_office['is-temporary-closed'] == true
 
       date_interval = I18n.t('custom_date_intervals.russian_post.intervals')
