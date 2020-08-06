e797ad9a03d91787940944ba3b909e075d7f462b
Polina Janochkina <apollinariya2209@yandex.ru>
2020-08-06 15:16:19 +0300

    Do not override_days_count for Moscow

--- app/models/concerns/dateable.rb
+++ app/models/concerns/dateable.rb
@@ -75,7 +75,7 @@ module Dateable
 
     case provider.name
     when 'Boxberry'
-      I18n.t(:hack, scope: %i[custom_date_intervals boxberry]).include?(deliverable_name)
+      courier? || I18n.t(:hack, scope: %i[custom_date_intervals boxberry]).include?(deliverable_name)
     end
   end
 
