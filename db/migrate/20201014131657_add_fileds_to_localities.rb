0c65acf905e5239be945fc91ac9f85600ce15030
Polina Janochkina <apollinariya2209@yandex.ru>
2020-10-27 11:57:42 +0300

    Update migration

--- db/migrate/20201014131657_add_fileds_to_localities.rb
+++ db/migrate/20201014131657_add_fileds_to_localities.rb
@@ -2,7 +2,6 @@ class AddFiledsToLocalities < ActiveRecord::Migration[6.0]
   def change
     add_column :localities, :latitude, :float
     add_column :localities, :longitude, :float
-    add_column :localities, :district, :string
     add_column :localities, :kladr_id, :string
   end
 end
