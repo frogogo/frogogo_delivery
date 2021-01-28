4a8a6fdb0e8219d538ec51242af26e91b6cc02f4
Polina Janochkina <apollinariya2209@yandex.ru>
2021-01-28 19:02:03 +0300

    Update DeliveryPointsController

--- app/controllers/delivery_points_controller.rb
+++ app/controllers/delivery_points_controller.rb
@@ -1,4 +1,17 @@
 class DeliveryPointsController < ApplicationController
   def index
+    @delivery_points = DeliveryPointsResolver.new(search_params).resolve
+
+    @delivery_points = @delivery_points
+      .joins(:delivery_method)
+      .active
+      .order(provider_id: :asc, date_interval: :asc)
+      .uniq { |delivery_point| [delivery_point.latitude, delivery_point.longitude] }
+  end
+
+  private
+
+  def search_params
+    params.permit(delivery_methods_ids: [])
   end
 end
