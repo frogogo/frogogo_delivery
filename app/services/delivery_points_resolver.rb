7fbaf47d643fd1b88437fdcf353c9f9f43461ce6
Polina Janochkina <apollinariya2209@yandex.ru>
2021-01-28 19:01:48 +0300

    Create DeliveryPointsResolver

---
+++ app/services/delivery_points_resolver.rb
@@ -0,0 +1,29 @@
+class DeliveryPointsResolver
+  def initialize(delivery_methods_ids)
+    @delivery_methods = DeliveryMethod.where(id: [delivery_methods_ids])
+    @locality = @delivery_methods.first.locality
+  end
+
+  def resolve
+    return if @delivery_methods.blank?
+    return if @locality.delivery_zone.inactive? || @locality.subdivision.delivery_zone.inactive?
+
+    result = @delivery_methods.map(&:delivery_points)
+
+    return result if result.present?
+
+    fetch_new_data
+  end
+
+  private
+
+  def fetch_new_data
+    case country.language_code.to_sym
+    when :ru
+      RU::BoxberryService.new(@locality).fetch_pickup_points
+      RU::RussianPostService.new(@locality).fetch_pickup_points
+
+      locality.delivery_methods.active
+    end
+  end
+end
