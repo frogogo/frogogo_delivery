ae10cf06062acdc2030d0fe895aa9db896263bb1
Dan Kim <itsdanya@gmail.com>
2021-02-01 13:50:26 +0300

    Remove country associations

--- app/models/delivery_zone.rb
+++ app/models/delivery_zone.rb
@@ -15,8 +15,6 @@
 #
 
 class DeliveryZone < ApplicationRecord
-  belongs_to :country
-
   enum zone: {
     default: 0, "1": 1, "2": 2, "3": 3, "4": 4,
     "5": 5, "6": 6, "7": 7, "8": 8, "9": 9
--- app/models/subdivision.rb
+++ app/models/subdivision.rb
@@ -15,7 +15,6 @@ class Subdivision < ApplicationRecord
   DEFAULT_NAME = 'Default'
 
   belongs_to :delivery_zone, optional: true
-  belongs_to :country
 
   has_many :delivery_methods, as: :deliverable, dependent: :destroy
   has_many :localities, dependent: :destroy
