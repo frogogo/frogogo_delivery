8d0a37909d89a445357a1af746645bd8bd60e42e
Dan Kim <itsdanya@gmail.com>
2021-02-01 13:55:08 +0300

    Remove Country from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,40 @@
-# Create countries
-# slovenia = Country.create!(iso_code: 'SI', language_code: 'sl', name: 'Slovenia')
-russia = Country.create!(iso_code: 'RU', language_code: 'ru', name: 'Россия')
-turkey = Country.create!(iso_code: 'TR', language_code: 'tr', name: 'Turkey')
-
 # Create delivery_zones
-DeliveryZone.create!(courier_fee: 299, pickup_fee: 199, free_delivery_gold_threshold: 1500, free_delivery_threshold: 2000, zone: 1, country: russia)
-DeliveryZone.create!(courier_fee: 349, pickup_fee: 249, free_delivery_gold_threshold: 1500, free_delivery_threshold: 2000, zone: 2, country: russia)
-DeliveryZone.create!(courier_fee: 399, pickup_fee: 299, free_delivery_gold_threshold: 2000, free_delivery_threshold: 2500, zone: 3, country: russia)
-DeliveryZone.create!(courier_fee: 499, pickup_fee: 399, free_delivery_gold_threshold: 2000, free_delivery_threshold: 2500, zone: 4, country: russia)
-DeliveryZone.create!(courier_fee: 799, pickup_fee: 699, free_delivery_gold_threshold: 4000, free_delivery_threshold: 4500, zone: 5, country: russia)
-DeliveryZone.create!(courier_fee: 7, pickup_fee: 7, post_fee: 7, free_delivery_gold_threshold: 100, free_delivery_threshold: 150, zone: :default, country: turkey)
+DeliveryZone.create!(
+  courier_fee: 299, pickup_fee: 199,
+  free_delivery_gold_threshold: 1500, free_delivery_threshold: 2000,
+  zone: 1
+)
+DeliveryZone.create!(
+  courier_fee: 349, pickup_fee: 249,
+  free_delivery_gold_threshold: 1500, free_delivery_threshold: 2000,
+  zone: 2
+)
+DeliveryZone.create!(
+  courier_fee: 399, pickup_fee: 299,
+  free_delivery_gold_threshold: 2000, free_delivery_threshold: 2500,
+  zone: 3
+)
+DeliveryZone.create!(
+  courier_fee: 499, pickup_fee: 399,
+  free_delivery_gold_threshold: 2000, free_delivery_threshold: 2500,
+  zone: 4
+)
+DeliveryZone.create!(
+  courier_fee: 799, pickup_fee: 699,
+  free_delivery_gold_threshold: 4000, free_delivery_threshold: 4500,
+  zone: 5
+)
+DeliveryZone.create!(
+  courier_fee: 7, pickup_fee: 7, post_fee: 7,
+  free_delivery_gold_threshold: 100, free_delivery_threshold: 150,
+  zone: :default
+)
 
 # Create subdivisions
-# istanbul = Subdivision.create!(iso_code: 'TR-34', local_code: '34', name: 'İstanbul', country: turkey)
-# ljubljana = Subdivision.create!(iso_code: 'SI-061', local_code: '61', name: 'Ljubljana', country: slovenia)
-tr_default_sub = Subdivision.create!(iso_code: '00', local_code: '00', name: 'Default', country: turkey, delivery_zone: turkey.delivery_zones.default.first)
-# moscow = Subdivision.create!(iso_code: 'RU-MOW', local_code: '45', name: 'Москва', country: russia, delivery_zone: russia.delivery_zones.find_by(zone: 1))
+# istanbul = Subdivision.create!(iso_code: 'TR-34', local_code: '34', name: 'İstanbul')
+# ljubljana = Subdivision.create!(iso_code: 'SI-061', local_code: '61', name: 'Ljubljana')
+# tr_default_sub = Subdivision.create!(iso_code: '00', local_code: '00', name: 'Default', delivery_zone: turkey.delivery_zones.default.first)
+# moscow = Subdivision.create!(iso_code: 'RU-MOW', local_code: '45', name: 'Москва', delivery_zone: russia.delivery_zones.find_by(zone: 1))
 
 # Create localities
 # Locality.create!(name: 'İstanbul', local_code: '212', postal_code: '34000', subdivision: tr_default_sub)
@@ -25,8 +44,10 @@ tr_default_sub = Subdivision.create!(iso_code: '00', local_code: '00', name: 'De
 # Create providers
 Provider.create!(code: 'russian_post', name: 'RussianPostPickup')
 Provider.create!(code: 'boxberry', name: 'Boxberry')
-Provider.create!(name: 'Turkey Post')
+# Provider.create!(name: 'Turkey Post')
 
 # Create delivery methods
-# DeliveryMethod.create!(method: :post, deliverable: moscow, provider: russian_post)
-DeliveryMethod.create!(method: :post, deliverable: tr_default_sub, provider: Provider.find_by(name: 'Turkey Post'))
+DeliveryMethod.create!(method: :post, deliverable: moscow, provider: russian_post)
+# DeliveryMethod.create!(
+#   method: :post, deliverable: tr_default_sub, provider: Provider.find_by(name: 'Turkey Post')
+# )
