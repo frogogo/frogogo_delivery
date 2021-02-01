2f471f851756d57d0e6bf96550d3128f18806edd
Dan Kim <itsdanya@gmail.com>
2021-02-01 15:48:12 +0300

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,8 +32,7 @@ DeliveryZone.create!(
 
 # Create subdivisions
 # istanbul = Subdivision.create!(iso_code: 'TR-34', local_code: '34', name: 'İstanbul')
-# ljubljana = Subdivision.create!(iso_code: 'SI-061', local_code: '61', name: 'Ljubljana')
-# tr_default_sub = Subdivision.create!(iso_code: '00', local_code: '00', name: 'Default', delivery_zone: turkey.delivery_zones.default.first)
+
 moscow = Subdivision.create!(
   iso_code: 'RU-MOW',
   local_code: '45',
@@ -44,15 +43,21 @@ moscow = Subdivision.create!(
 # Create localities
 # Locality.create!(name: 'İstanbul', local_code: '212', postal_code: '34000', subdivision: tr_default_sub)
 # Locality.create!(name: 'Ljubljana', local_code: '3861', postal_code: '1000', subdivision: ljubljana)
-# Locality.create!(name: 'Москва', local_code: '45000000', postal_code: '101000', delivery_zone: russia.delivery_zones.find_by(zone: 1), subdivision: moscow)
+
+Locality.create!(
+  name: 'Москва',
+  local_code: '45000000',
+  postal_code: '101000',
+  delivery_zone: DeliveryZone.find_by(zone: 1),
+  subdivision: moscow
+)
 
 # Create providers
 russian_post = Provider.create!(code: 'russian_post', name: 'RussianPostPickup')
-Provider.create!(code: 'boxberry', name: 'Boxberry')
+boxberry = Provider.create!(code: 'boxberry', name: 'Boxberry')
 # Provider.create!(name: 'Turkey Post')
 
 # Create delivery methods
-DeliveryMethod.create!(method: :post, deliverable: moscow, provider: russian_post)
-# DeliveryMethod.create!(
-#   method: :post, deliverable: tr_default_sub, provider: Provider.find_by(name: 'Turkey Post')
-# )
+DeliveryMethod.create!(method: :pickup, deliverable: moscow, provider: russian_post)
+DeliveryMethod.create!(method: :pickup, deliverable: moscow, provider: boxberry)
+DeliveryMethod.create!(method: :courier, deliverable: moscow, provider: boxberry)
