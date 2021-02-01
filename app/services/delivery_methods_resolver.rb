847fc6cef97675d1faa9f04cad492c23db065d57
Dan Kim <itsdanya@gmail.com>
2021-02-01 16:34:45 +0300

    DeliveryMethodsResolver: remove create_locality_and_subdivision

--- app/services/delivery_methods_resolver.rb
+++ app/services/delivery_methods_resolver.rb
@@ -7,7 +7,7 @@ class DeliveryMethodsResolver
 
   def resolve
     return if @locality.delivery_zone.blank?
-    return if @locality.delivery_zone.inactive?
+    return if @locality.delivery_zone.inactive? || @locality.subdivision.delivery_zone.inactive?
 
     delivery_methods = @locality.delivery_methods
 
@@ -23,33 +23,13 @@ class DeliveryMethodsResolver
 
   attr_reader :locality_name, :subdivision, :subdivision_name, :latitude, :longitude, :locality_uid
 
-  def create_locality_and_subdivision
-    @subdivision = Subdivision.create_or_find_by!(
-      name: subdivision_name,
-      delivery_zone: region_delivery_zone(subdivision_name)
-    )
-    @locality = Locality.create_or_find_by!(
-      name: locality_name,
-      subdivision: subdivision,
-      latitude: latitude,
-      longitude: longitude,
-      locality_uid: locality_uid,
-      delivery_zone: city_delivery_zone(subdivision_name, locality_name) ||
-        subdivision.delivery_zone
-    )
-  end
-
   def fetch_new_data
     case I18n.locale
     when :ru
-      create_locality_and_subdivision
+      RU::BoxberryService.new(@locality).fetch_delivery_methods
+      RU::RussianPostService.new(@locality).fetch_delivery_method
 
-      return if locality.delivery_zone.inactive? || subdivision.delivery_zone.inactive?
-
-      RU::BoxberryService.new(locality).fetch_delivery_methods
-      RU::RussianPostService.new(locality).fetch_delivery_method
-
-      locality.delivery_methods.active
+      @locality.delivery_methods.active
     end
   end
 
