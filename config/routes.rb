c581a32620ab8cf1a30402a7f9d46a8b7b36940a
Polina Janochkina <apollinariya2209@yandex.ru>
2021-02-02 18:26:47 +0300

    Routes: nested delivery_points

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   defaults format: :json do
-    resources :delivery_methods, only: %i[index]
-    resources :delivery_points, only: %i[index]
+    resources :delivery_methods, only: %i[index] do
+      resources :delivery_points, only: %i[index]
+    end
     resource :delivery_zone, only: %i[show]
   end
 end
