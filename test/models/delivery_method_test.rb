cf32ddbf29f7578eff690287cac120fd3da09c4e
Polina Janochkina <apollinariya2209@yandex.ru>
2021-02-03 14:32:57 +0300

    Add test 'should update delivery_methods_updated_at'

--- test/models/delivery_method_test.rb
+++ test/models/delivery_method_test.rb
@@ -2,10 +2,20 @@ require 'test_helper'
 
 class DeliveryMethodTest < ActiveSupport::TestCase
   setup do
-    @delivery_method = delivery_methods(:turkey_post)
+    @delivery_method = delivery_methods(:russian_post)
   end
 
   test 'valid' do
     assert @delivery_method.valid?
+  end
+
+  test 'should update delivery_methods_updated_at' do
+    locality = @delivery_method.deliverable
+
+    assert_nil locality.delivery_methods_updated_at
+
+    @delivery_method.update!(date_interval: '2-3')
+
+    assert locality.delivery_methods_updated_at.present?
   end
 end
