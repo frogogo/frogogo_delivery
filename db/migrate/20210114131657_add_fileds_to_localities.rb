8cc286b31b3592a946f4b7a82fe75625026b16f8
Polina Janochkina <apollinariya2209@yandex.ru>
2021-02-05 16:39:51 +0300

    Temp: remove migration

--- db/migrate/20210114131657_add_fileds_to_localities.rb
+++
@@ -1,7 +0,0 @@
-class AddFiledsToLocalities < ActiveRecord::Migration[6.0]
-  def change
-    add_column :localities, :latitude, :float
-    add_column :localities, :longitude, :float
-    add_column :localities, :kladr_id, :string
-  end
-end
