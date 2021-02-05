403d1c6045045f0201578c9c9c6f11d46325ee42
Polina Janochkina <apollinariya2209@yandex.ru>
2021-02-05 17:43:29 +0300

    Add if_not_exists option

--- db/migrate/20210114131657_add_fileds_to_localities.rb
+++ db/migrate/20210114131657_add_fileds_to_localities.rb
@@ -1,7 +1,7 @@
 class AddFiledsToLocalities < ActiveRecord::Migration[6.0]
   def change
-    add_column :localities, :latitude, :float
-    add_column :localities, :longitude, :float
-    add_column :localities, :kladr_id, :string
+    add_column :localities, :latitude, :float, if_not_exists: true
+    add_column :localities, :longitude, :float, if_not_exists: true
+    add_column :localities, :kladr_id, :string, if_not_exists: true
   end
 end
