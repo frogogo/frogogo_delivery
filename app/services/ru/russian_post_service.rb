364967be45415cacb9ddfcc6acb9d0712c1de345
Polina Janochkina <apollinariya2209@yandex.ru>
2021-02-05 15:04:11 +0300

    Handle ArgumentError (Empty list of attributes passed)

--- app/services/ru/russian_post_service.rb
+++ app/services/ru/russian_post_service.rb
@@ -37,6 +37,8 @@ class RU::RussianPostService < DeliveryService
       .select { |post_office| post_office.region.downcase.include?(canonical_subdivision_name) }
       .map { |post_office| post_office.to_attributes(date_interval, @provider.id) }
 
+    return if delivery_points_attributes.blank?
+
     delivery_method.delivery_points
       .insert_all(delivery_points_attributes)
     delivery_method.update!(date_interval: date_interval)
