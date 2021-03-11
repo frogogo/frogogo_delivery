e5b27332421cc0783e9758079b345d2cf06fb51b
Polina Janochkina <apollinariya2209@yandex.ru>
2021-03-11 15:48:11 +0300

    Remove #locality_suggestion

--- app/controllers/delivery_methods_controller.rb
+++ app/controllers/delivery_methods_controller.rb
@@ -26,27 +26,27 @@ class DeliveryMethodsController < ApplicationController
   private
 
   def set_locality
-    locality_uid = params[:locality_uid] || locality_params.kladr_id
+    return nil if dadata_suggestion.nil?
+
+    locality_uid = params[:locality_uid] || dadata_suggestion.kladr_id
 
     @locality = Locality.find_by(locality_uid: locality_uid)
-    @locality = Locality.create!(locality_params.locality_attributes) if @locality.blank?
+    @locality = Locality.create!(dadata_suggestion.locality_attributes) if @locality.blank?
     @locality = @locality.parent_locality if @locality.parent_locality.present?
-  end
-
-  def locality_params
-    @locality_params ||= DaDataSuggestion.new(dadata_suggestion)
   end
 
   def dadata_suggestion
     dadata = DaDataService.instance
 
-    if params[:locality_uid].present?
-      dadata.suggestion_from_locality_uid(params[:locality_uid])
-    else
-      dadata.suggestion_from_locality_and_subdivision(
-        params[:locality],
-        params[:subdivision]
-      )
-    end
+    @dadata_suggestion ||= if params[:locality_uid].present?
+                             dadata.suggestion_from_locality_uid(params[:locality_uid])
+                           else
+                             dadata.suggestion_from_locality_and_subdivision(
+                               params[:locality],
+                               params[:subdivision]
+                             )
+                           end
+
+    DaDataSuggestion.new(@dadata_suggestion) if @dadata_suggestion.present?
   end
 end
--- app/controllers/delivery_zones_controller.rb
+++ app/controllers/delivery_zones_controller.rb
@@ -14,27 +14,25 @@ class DeliveryZonesController < ApplicationController
   def set_locality
     return nil if dadata_suggestion.nil?
 
-    locality_uid = params[:locality_uid] || locality_suggestion.kladr_id
+    locality_uid = params[:locality_uid] || dadata_suggestion.kladr_id
 
     @locality = Locality.find_by(locality_uid: locality_uid)
-    @locality = Locality.create!(locality_suggestion.locality_attributes) if @locality.blank?
+    @locality = Locality.create!(dadata_suggestion.locality_attributes) if @locality.blank?
     @locality = @locality.parent_locality if @locality.parent_locality.present?
-  end
-
-  def locality_suggestion
-    @locality_suggestion ||= DaDataSuggestion.new(dadata_suggestion)
   end
 
   def dadata_suggestion
     dadata = DaDataService.instance
 
-    if params[:locality_uid].present?
-      dadata.suggestion_from_locality_uid(params[:locality_uid])
-    else
-      dadata.suggestion_from_locality_and_subdivision(
-        params[:locality],
-        params[:subdivision]
-      )
-    end
+    @dadata_suggestion ||= if params[:locality_uid].present?
+                             dadata.suggestion_from_locality_uid(params[:locality_uid])
+                           else
+                             dadata.suggestion_from_locality_and_subdivision(
+                               params[:locality],
+                               params[:subdivision]
+                             )
+                           end
+
+    DaDataSuggestion.new(@dadata_suggestion) if @dadata_suggestion.present?
   end
 end
