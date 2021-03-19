fcc162f7248c3f63fd7b6fc3167b407c3f7ba781
Polina Janochkina <apollinariya2209@yandex.ru>
2021-03-19 17:14:45 +0300

    Add test 'should not duplicate delivery_point'

--- test/models/delivery_point_test.rb
+++ test/models/delivery_point_test.rb
@@ -8,4 +8,8 @@ class DeliveryPointTest < ActiveSupport::TestCase
   test 'valid' do
     @delivery_point.valid?
   end
+
+  test 'should not duplicate delivery_point' do
+    assert @delivery_point.dup.invalid?
+  end
 end
