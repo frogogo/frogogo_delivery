db90be088c91636c3b8c1ebcef3e8ae3c305edd0
Polina Janochkina <apollinariya2209@yandex.ru>
2021-04-07 15:41:17 +0300

    Creat FivePostService

---
+++ app/services/ru/five_post_point.rb
@@ -0,0 +1,52 @@
+class RU::FivePostPoint
+  WORDS_TO_REMOVE = %w[город область край республика автономный округ г]
+
+  def initialize(params)
+    @full_address = params['fullAddress']
+    @latitude = params['address']['lat']
+    @longitude = params['address']['lng']
+    @postal_code = params['address']['zipCode']
+    @region = params['address']['region']
+    @city = params['address']['city']
+    @working_hours = params['workHours']
+    @date_interval = params['deliverySL']&.first
+    @short_address = params['shortAddress']
+  end
+
+  def to_attributes(provider_id)
+    {
+      address: @full_address,
+      code: @postal_code,
+      created_at: Time.current,
+      date_interval: date_interval,
+      latitude: @latitude,
+      longitude: @longitude,
+      name: name,
+      provider_id: provider_id,
+      updated_at: Time.current,
+      working_hours: working_hours
+    }
+  end
+
+  def name
+    "Постамат Пяторочки #{@short_address}"
+  end
+
+  def working_hours
+    "C #{@working_hours.first['opensAt']} до #{@working_hours.first['closesAt']}"
+  end
+
+  def canonical_region_name
+    @region.split.delete_if { |word| WORDS_TO_REMOVE.include?(word.downcase) }.join(' ').downcase
+  end
+
+  def canonical_city_name
+    @city.split.delete_if { |word| WORDS_TO_REMOVE.include?(word.downcase) }.join(' ').downcase
+  end
+
+  def date_interval
+    return nil if @date_interval.nil?
+
+    @date_interval['sl']
+  end
+end
---
+++ app/services/ru/five_post_service.rb
@@ -0,0 +1,35 @@
+class RU::FivePostService < DeliveryService
+  FIVE_POST_NAME = 'FivePost'
+
+  def initialize(locality, delivery_method: nil)
+    super
+
+    @delivery_service = RU::FivePostAdapter.new(locality)
+    @provider = Provider.find_by(name: FIVE_POST_NAME)
+    @subdivision_name = locality.subdivision.name
+    @delivery_method = delivery_method
+  end
+
+  def fetch_delivery_methods
+    DeliveryMethod.create_or_find_by!(
+      method: :pickup,
+      deliverable: locality
+    )
+  end
+
+  def fetch_pickup_points(delivery_method)
+    return unless super
+
+    @response = delivery_service.pickup_ponit_list
+byebug
+    delivery_points_attributes = response.map { |params| RU::FivePostPoint.new(params) }
+      .select { |five_post| five_post.canonical_city_name == locality.name.downcase }
+      .select { |five_post| five_post.canonical_region_name == @subdivision_name.downcase }
+      .map { |five_post| five_post.to_attributes(@provider.id) }
+
+    return if delivery_points_attributes.blank?
+
+    delivery_method.delivery_points
+      .insert_all(delivery_points_attributes)
+  end
+end
