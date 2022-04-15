dbad7f332712dad8aabd72d7e5d8f15d3727ea71
Dan Kim <itsdanya@gmail.com>
2022-04-15 22:20:27 +0200

    Fix NoMethodError migration

--- db/migrate/20201021105426_add_inactive_to_delivery_zones.rb
+++ db/migrate/20201021105426_add_inactive_to_delivery_zones.rb
@@ -2,6 +2,6 @@ class AddInactiveToDeliveryZones < ActiveRecord::Migration[6.0]
   def change
     add_column :delivery_zones, :inactive, :boolean, default: false
 
-    DeliveryZone.find_by(zone: '6').update_column(:inactive, true)
+    DeliveryZone.find_by(zone: '6')&.update_column(:inactive, true)
   end
 end
